48e8b0089459ff4664031ede9716b3c03adfecc2
prathamesh.pawar <prathamesh.pawar@blancco.com>
2024-08-24 07:33:34 +0530

    Initial Commit

---
+++ MySettings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySettings
+//
+//  Created by Prathamesh Pawar on 8/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
